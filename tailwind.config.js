d59e39f0de061fe9575d31252f49fad48461d29b
Sandro Scalco <sandro.scalco@liitu.ch>
2021-06-02 21:08:01 +0200

    feat input text

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const { guessProductionMode } = require("@ngneat/tailwind");
+
+module.exports = {
+    prefix: '',
+    purge: {
+      enabled: guessProductionMode(),
+      content: [
+        './src/**/*.{html,ts}',
+      ]
+    },
+    darkMode: 'media', // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
+};
